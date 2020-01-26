4ada7b01cec55e221a5892caf1087cf5b267e1f3
JCSteiner <59900699+JCSteiner@users.noreply.github.com>
2020-01-25 20:47:57 -0500

    Add files via upload
    object oriented

--- Neural Networks/Neuron.py
+++ Neural Networks/Neuron.py
@@ -1,94 +1,64 @@
 ###############################################################################
 #                                                                             #
-#                                    Neuron.py                                #
+#                                   Neuron.py                                 #
 #                                                                             #
-#                                    J. Steiner                               #
+#                                   J. Steiner                                #
 #                                                                             #
 ###############################################################################
 
-#%%############################################################################
-#                                                                             #
-#                                Nueron Class                                 #
-#                                                                             #
-###############################################################################
+#Imports the ability to easily work with matricies
+import numpy as np
+
+#A global function for determining neuron output
+def sigmoid(z):
+    #The sigmoid function value of z
+    return 1.0/(1.0+np.exp(-z))
+
+#Neruon class
 class Neuron:
     
-    ###########################################################################
-    #                                                                         #
-    # Name:       __init__                                                    #
-    # Parameters: N/A                                                         #
-    # Reutrns:    N/A                                                         #  
-    # Notes:      Class constructor, defaults instance variables: weights,    #
-    #             bias, input, output                                         #
-    #                                                                         #
-    ###########################################################################
-    
-    def __init__(self):
+    #Neuron class constructor
+    def __init__(self, numInputs):
         
-        #Defaults instance variables
-        self.weights, self.bias = ( None, None )
-        self.input, self.output = ( None, None )
+        #Initializes random weights from a uniform distribution with a range
+        #of 0, 1 as a numpy array with 1 row and a column for each input the
+        #neuron will recieve
+        self.weights = np.random.uniform(size = numInputs)
         
-    ###########################################################################
-    #                                                                         #
-    # Name:       evaluate                                                    #
-    # Parameters: inputs - the feature vector and classification              #
-    # Reutrns:    True or False, if the neuron is activated                   #  
-    # Notes:      test data should have a classification of None              #
-    #                                                                         #
-    ###########################################################################
+        #Initializes a random bias from a 
+        self.bias    = np.random.uniform()
         
-    def evaluate(self, inputs):
+        #Defaults an output to be -1, this is because since range of possible
+        #outputs is in the interval [0, 1], -1 is treated as an indication that
+        #the neuron output was not calculated
+        self.output  = -1
         
-        #Stores the feature vector into variable x
-        x           = inputs[0]
-
-        #Computes the element-wise multiplication of the features and weights
-        weightedInput  = [ x[i] * self.weights[i] for i in range(len(x)) ]
+    #Calculates neuron output
+    def evaluate(self, data):
         
-        #Computes the dot product of features and weights, adds bias
-        self.output = sum( weightedInput ) + self.bias
+        #Calculates the value z as the wieghted input plus the neuron bias
+        z = np.dot(self.weights, data) + self.bias
         
-        #Returns if the output is greater than the threshold of 0
-        return self.output >= 0
-    
-    ###########################################################################
-    #                                                                         #
-    # Name:       setWeights                                                  #
-    # Parameters: newWeights - list of new weights                            #
-    # Reutrns:    N/A                                                         #  
-    # Notes:      Setter method, newWeights should be a list                  #
-    #                                                                         #
-    ###########################################################################
-    
-    def setWeights(self, newWeights):
+        #Calculates the neuron output to be the sigmoid function of z
+        self.output = sigmoid(z)
         
-        #Sets new weights
-        self.weights = newWeights
-    
-    ###########################################################################
-    #                                                                         #
-    # Name:       setBias                                                     #
-    # Parameters: newBias - the new bias                                      #
-    # Reutrns:    N/A                                                         #  
-    # Notes:      Setter method, newBias should be an integer or float        #
-    #                                                                         #
-    ###########################################################################
-    
-    def setBias(self, newBias):
-        
-        self.bias    = newBias 
-        
-    ###########################################################################
-    #                                                                         #
-    # Name:       getOutput                                                   #
-    # Parameters: N/A                                                         #
-    # Reutrns:    N/A                                                         #  
-    # Notes:      Getter method, public access to private variable called     #
-    #             output                                                      #
-    #                                                                         #
-    ###########################################################################
-        
-    def getOutput(self):
-        
+        #Returns the neuron output
         return self.output
+        
+    #Updates weights given the values deltaWeights
+    def updateWeights(self, deltaWeights, eta = 0.1):
+        
+        #Updates the weights by the corresponding deltaWeight*eta
+        #eta being the learning rate which is defualted to 0.1
+        self.weights -= eta * deltaWeights
+        
+    #Updates bias given the value deltaBias
+    def updateBias(self, deltaBias, eta = 0.1):
+        
+        #Updates the neuron's bias by the corresponding deltaBias*eta
+        #eta being the learning rate which is defaulted to 0.1
+        self.bias    -= eta * deltaBias
+    
+    #Gets the neuron output
+    def getOutput(self): return self.output
+    
