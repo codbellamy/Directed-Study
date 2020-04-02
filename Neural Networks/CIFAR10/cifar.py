044c4617211bf88eda7109ed62ff4190adabd6d7
Cody <codbellamy@gmail.com>
2020-04-02 12:47:39 -0700

    CIFAR CNN Classifier
    PyTorch CNN to classify CIFAR10. 2x2 will max at 62% accuracy after 107 epochs. Currently testing fractional max pooling.

---
+++ Neural Networks/CIFAR10/cifar.py
@@ -0,0 +1,139 @@
+#Loads necessary (and probably some unnecessary modules)
+import numpy as np
+import pickle
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+# Constants
+TYPE = '80_75_FMP'
+PATH = './states/'+TYPE+'/cifar_net_'            # Path and name of NN save states
+STATE_NAME = './states/'+TYPE+'/cifar_net_1'     # Name of state to load
+EPOCHS = 5                                       # Number of epochs of training
+LOAD_FLAG = False                                # Load from a previous state
+
+# Set up data for training and testing
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Create the CNN and Linear NN structure
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.fracpool1 = nn.FractionalMaxPool2d(3, output_ratio=.8)
+        # self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fracpool2 = nn.FractionalMaxPool2d(2, output_ratio=.75)
+        self.fc1 = nn.Linear(16 * 13 * 13, 1000)
+        self.fc2 = nn.Linear(1000, 100)
+        self.fc3 = nn.Linear(100, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.fracpool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.fracpool2(x)
+        x = x.view(-1, 16 * 13 * 13)   # 16 * 5 * 5
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# Create the network
+net = Net()
+if(LOAD_FLAG):
+    net.load_state_dict(torch.load(PATH))
+
+# NN overall assessment
+def assessment(epoch):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
+
+    torch.save(net.state_dict(), PATH+str(epoch))
+
+def classAssessment():
+    # Load test data
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # NN class assessment
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+
+# Optimize and define parameters of NN
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+net.train(True)
+
+for epoch in range(EPOCHS):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+    
+    # Assess state
+    assessment(epoch+1)
+    if(epoch%5==4):
+        classAssessment()
+
+print('Finished Training')
