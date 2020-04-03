da16b3243fe3b19782f6f245593f77077a1c761e
Cody <codbellamy@gmail.com>
2020-04-03 10:02:33 -0700

    Training Files

---
+++ Neural Networks/CIFAR10/trainer.py
@@ -0,0 +1,202 @@
+#Loads necessary (and probably some unnecessary modules)
+import numpy as np
+import pickle
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+# Adjustable parameters
+TYPE = '3xConv_MaxPool'
+LAST_STATE = 26                                 # Last network state that was saved
+TRAINING_EPOCHS = 100                            # Number of epochs of training
+LOAD_FLAG = True                                 # Load from a previous state
+TRAINING_FLAG = False                            # Whether or not to train the network, must not be false if ASSESSMENT_MODE is 0 or 1
+ASSESSMENT_MODE = 2                             # 0-None, 1-During Training, 2-Quick Assessment
+
+# Non-adjustable parameters
+PATH = './states/'+TYPE+'/cifar_net_'            # Path and name of NN save states
+STATE_NAME = './states/'\
+    +TYPE+'/cifar_net_'+str(LAST_STATE)         # Name of state to load
+
+# Set up data for training and testing
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+# Normalize the test set same as training set without augmentation
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Create the CNN and Linear NN structure
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv_layer = nn.Sequential(
+
+            # Conv Layer block 1
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Conv Layer block 2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.05),
+
+            # Conv Layer block 3
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.1),
+            nn.Linear(4096, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.1),
+            nn.Linear(512, 10)
+        )
+
+
+    def forward(self, x):
+        """Perform forward."""
+        
+        # conv layers
+        x = self.conv_layer(x)
+        
+        # flatten
+        x = x.view(x.size(0), -1)
+        
+        # fc layer
+        x = self.fc_layer(x)
+
+        return x
+
+# NN overall assessment
+def assessment(epoch):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
+
+    if(epoch is not 0):
+        torch.save(net.state_dict(), PATH+str(epoch))
+
+def classAssessment():
+    # Load test data
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # NN class assessment
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+
+def training(epoch_start, epoch_end):
+    for epoch in range(epoch_start, epoch_end):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+        
+        # Assess state
+        if(ASSESSMENT_MODE == 1):
+            assessment(epoch+1)
+            if(epoch%5==4):
+                classAssessment()
+        else:
+            torch.save(net.state_dict(), PATH+str(epoch+1))
+
+    print('Finished Training')
+
+# Create the network
+net = Net()
+if(LOAD_FLAG):
+    net.load_state_dict(torch.load(STATE_NAME))
+
+# Optimize and define parameters of NN
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+if(TRAINING_FLAG and ASSESSMENT_MODE is not 2):
+    net.train(True)
+    training(LAST_STATE, TRAINING_EPOCHS)
+elif(ASSESSMENT_MODE == 2):
+    assessment(0)
+    classAssessment()
+else:
+    print('Error: Incorrect parameters for TRAINING_FLAG and ASSESSMENT_MODE')
